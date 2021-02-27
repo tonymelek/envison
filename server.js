e89751205d263f259000c9750167098bc6ea3e25
Tony Melek <tonymelek.au@gmail.com>
2021-02-27 23:30:05 +1100

    modify dotenv

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const requireEnvConfig = require('./dotenv/config')
-
-
-
 // Define middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +10,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 } else {
+  const requireEnvConfig = require('./dotenv/config')
   app.use(express.static("client/public"))
 }
 
